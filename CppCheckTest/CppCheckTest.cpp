89e239d656a877c928b1fdc26ec731a4e1985789
Julian Becker <julian.becker@faroeurope.com>
2018-06-04 19:37:47 +0200

    Project with cppcheck and clang-tidy analysis

---
+++ CppCheckTest/CppCheckTest.cpp
@@ -0,0 +1,17 @@
+// CppCheckTest.cpp: Definiert den Einstiegspunkt für die Konsolenanwendung.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <map>
+#include <windows.h>
+
+int main(int argc, char** argv)
+{
+	int y;
+	std::map<int, int> m;
+	int * v;
+	std::cout << *v << std::endl;
+	return  y;
+}
+
---
+++ CppCheckTest/Quelle.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int testfun()
+{
+	printf("helo\n",10);
+	return 0;
+}
